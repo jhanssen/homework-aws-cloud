3fffa5c44f84242cbf98c6956f1e6cddcfbb6709
Jan Erik Hanssen <jhanssen@gmail.com>
2016-05-12 02:13:29 -0700

    fix hopefully

--- src/server.js
+++ src/server.js
@@ -198,7 +198,7 @@ app.use(session({
 app.use(passwordless.sessionSupport());
 
 app.use(function(req, res, next) {
-    if(req.protocol !== 'https' && req.protocol !== 'wss') {
+    if (req.protocol !== 'https' && req.headers["x-forwarded-proto"] !== 'https') {
         console.log("invalid protocol attempt", req.protocol, req.headers);
         res.status(403).send({message: 'SSL required'});
         return;
