0ce675214bd48db65838c86d06d9a633063f3b8b
Anders Bakken <agbakken@gmail.com>
2017-04-07 14:38:52 -0700

    fixes

--- bin/run.sh
+++ bin/run.sh
@@ -3,11 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../src
 while true; do
-    RESTART=
+    RESTART=1
     ./server.js
     if [ ! "$?" -eq 0 ]; then
         /bin/mv log crash.log.`date +%s`
-        RESTART=1
+        RESTART=
     fi
     if [ -e '.deploy.pull' ]; then
         rm ".deploy.pull"
